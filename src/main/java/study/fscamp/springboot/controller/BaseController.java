7e12afca6657687f07d8d1fa6b4b3db908a4a91a
jhkang1313 <jhkang1313@gmail.com>
2024-03-24 01:23:14 +0900

    #2 - view Controller
    뷰 컨트롤러 작성

--- src/main/java/study/fscamp/springboot/controller/BaseController.java
+++ src/main/java/study/fscamp/springboot/controller/BaseController.java
@@ -1,14 +1,19 @@
 package study.fscamp.springboot.controller;
 
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class  BaseController {
-
-  /*@GetMapping("/")
+public class  BaseController implements ErrorController {
+  @GetMapping("/")
   public String root() {
     return "index";
-  }*/
+  }
 
+  @RequestMapping("/error")
+  public String error(){
+    return "error";
+  }
 }
