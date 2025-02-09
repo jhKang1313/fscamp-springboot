ad7e1371bdf2458bdfd4a1728306a55e531deb88
jhkang1313 <jhkang1313@gmail.com>
2025-02-10 01:22:12 +0900

    P2C4_3 Spring Cache, Redis 실습

--- src/main/java/study/fscamp/springboot/FscampSpringbootApplication.java
+++ src/main/java/study/fscamp/springboot/FscampSpringbootApplication.java
@@ -1,13 +1,29 @@
 package study.fscamp.springboot;
 
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.event.EventListener;
+import study.fscamp.springboot.service.StudentService;
 
+@RequiredArgsConstructor
 @SpringBootApplication
 public class FscampSpringbootApplication {
+
+	private final StudentService studentService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(FscampSpringbootApplication.class, args);
 	}
 
+	@EventListener(ApplicationReadyEvent.class)
+	public void init() {
+		studentService.printStudent("jack");
+		studentService.printStudent("jack");
+		studentService.printStudent("jack");
+		studentService.printStudent("jack");
+	}
 }
---
+++ src/main/java/study/fscamp/springboot/repository/StudentRepository.java
@@ -0,0 +1,24 @@
+package study.fscamp.springboot.repository;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Repository;
+import study.fscamp.springboot.domain.Student;
+
+import java.util.Map;
+
+@RequiredArgsConstructor
+@Repository
+public class StudentRepository {
+  private final Map<String, Student> storage;
+  @Cacheable("student")
+  public Student getStudent(String name) {
+    System.out.println("[repo] 나의 통행료는 무척 비싸다!");
+    return storage.get(name);
+  }
+  public Student saveStudent(String name, Integer age, Student.Grade grade) {
+    return storage.put(name, Student.of(name, age, grade));
+  }
+}
